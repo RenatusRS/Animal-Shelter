0715ee410536be558744387b171da6faa1ff1f07
djordjeds <djordjedstanojevic@gmail.com>
2022-06-16 21:02:41 +0200

    promena alignment

--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
 function load_user() {
-    if (document.getElementById("user_id").value == "hose" || document.getElementById("pass_id").value == "hose") {
+    if (document.getElementById("user_id").value == "hose" && document.getElementById("pass_id").value == "hose") {
         var korisnik= document.getElementById("user_id").value;
         localStorage.setItem("korisnik", korisnik);
         window.location.href = 'index.html';
