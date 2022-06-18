b5efde1fbc688fa57d3114d6801f1e67f4e1c88c
djordjeds <djordjedstanojevic@gmail.com>
2022-06-18 20:55:37 +0200

    dodate pretrage i osobine

---
+++ js/psi.js
@@ -0,0 +1,36 @@
+function searchDog(){
+    var ulaz = document.getElementById('dog_filter').value.toLowerCase();
+
+    var kartice=document.getElementById('card-deck');
+    var kartice_arr=document.getElementsByClassName('col-lg-4 mb-4');
+
+
+    for(let i=0;i<kartice_arr.length;i++){
+        let title=kartice_arr[i].querySelector(".card .card-body h5.card-title");
+
+        if(title.innerText.toLowerCase().indexOf(ulaz) > -1){
+            kartice_arr[i].style.display="";
+        }
+        else{
+            kartice_arr[i].style.display="none";
+        }
+    }
+    
+}
+
+$(document).ready(function(){
+    $('#btnSort').click(function (){
+        $('.card-deck .card').sort(function(a,b) {
+            return $(a).find(".card-title").text() > $(b).find(".card-title").text() ? 1 : -1;
+        }).appendTo(".card-deck");
+    })
+
+    $('#btnSort2').click(function (){
+        $('.card-deck .card').sort(function(a,b) {
+            return $(a).find(".card-title").text() < $(b).find(".card-title").text() ? 1 : -1;
+        }).appendTo(".card-deck");
+    })
+
+
+
+  });
