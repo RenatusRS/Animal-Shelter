436af935a2156d518e44c162159914c7a4b5cf6c
djordjeds <djordjedstanojevic@gmail.com>
2022-07-03 16:44:31 +0200

    O nama i slike

--- js/login.js
+++ js/login.js
@@ -2,9 +2,8 @@ $(function () {
     $("#button_login").click(function () {
         for (let i = 0; i < users.length; i++) {
             if ($("#user_id").val() == users[i]["username"]) {
-                console.log("TEST");
                 if ($("#pass_id").val() != users[i]["password"]) break;
-
+                
                 localStorage.setItem("logged", i);
                 window.location.href = "index.html";
             }
